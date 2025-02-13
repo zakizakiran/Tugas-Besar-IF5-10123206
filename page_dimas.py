1fac8eed4eba79875112a2fb4af40170bf067cdb
DmsHrgi <dmz.hergi@gmail.com>
2025-02-13 19:50:47 +0700

    tolooong

--- page_dimas.py
+++ page_dimas.py
@@ -3,22 +3,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from variable import cleaned_data
 
 # Membuat tab sebagai navbar
 tab1, tab2 = st.tabs(["Pertanyaan", "Hasil"])
 
 with tab1:
-    st.markdown("""
-                <h2 style="color: #55AD9B;">Pertanyaan 2</h2>
-        """, unsafe_allow_html=True)
+    st.title("Pertanyaan 1")
     st.write("Muhamad Dimas Hergi P. - 10123211")
-    st.write("Apakah ada bulan dengan lonjakan peminjaman yang signifikan?")
+    st.write("Faktor-faktor apa yang paling berkorelasi dengan jumlah peminjaman sepeda?")
 
 with tab2:
-    st.markdown("""
-                <h2 style="color: #55AD9B;">Hasil Analisis</h2>
-        """, unsafe_allow_html=True)
-    st.write("Informasi tentang aplikasi ini.")
-    st.write("halo.")
-    st.write("hali.")
+    fig, ax = plt.subplots(figsize=(12, 6))
+    sns.boxplot(data=cleaned_data, x='hr', y='cnt', ax=ax)
+    ax.set_title("Distribusi Penyewaan Sepeda per Jam")
+    ax.set_xlabel("Jam")
+    ax.set_ylabel("Jumlah Penyewaan")
+    st.pyplot(fig)
+
+
 
