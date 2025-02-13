3877f880e0c8046bb43808ef38a6edf5cd6e1ca0
zakizakiran <zzakiran4@gmail.com>
2025-02-13 22:07:30 +0700

    updated

--- page_airin.py
+++ page_airin.py
@@ -1,12 +1,6 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import pydeck as pdk
-import geopandas as gpd
-from shapely.geometry import Point
-import random
 from variable import cleaned_data
 
 # Membuat tab sebagai navbar
